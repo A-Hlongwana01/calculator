e611464a0293ef2bc4cac2097c4a2f643fc8ab87
ahlongwa021 <ahlongwa021@gmail.co.za>
2023-02-27 15:00:55 +0200

    added buttons functionality of adding button text to textArea

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -18,11 +18,28 @@ const equalButton = document.getElementById("equals");
 const oBracketButton = document.getElementById("o-bracket"); 
 const cBracketButton = document.getElementById("c-bracket");
 
-const textArea = document.getElementById("input-text");
+// const textArea = document.getElementById("input");
 
-const currentResultOutPut = document.getElementById("input").value
+// const currentResultOutPut = document.getElementById("input").value
+// equalButton.addEventListener("click", addToTextArea)
 
 
-function addToTextArea(){
-    
-}
+const buttons = document.querySelectorAll('.btn')
+const textarea = document.querySelector('textarea')
+
+const delete_btn = document.querySelector('.erase-button')
+
+let chars = []
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        textarea.value += btn.innerText
+        chars = textarea.value.split('')
+    })
+})
+
+delete_btn.addEventListener('click', () => {
+    chars.pop()
+    textarea.value = chars.join('')
+})
+
